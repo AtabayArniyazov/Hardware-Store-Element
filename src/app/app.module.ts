dbca575ed74ee9abfa549a1922ac932c8958aab6
Atabay <Atabaj.Arniyazov@p97.com>
2021-09-22 00:54:05 +0300

    add HttpClientModule, create AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {HsHeaderComponent} from './hs-header/hs-header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HsHomePageComponent} from './hs-home-page/hs-home-page.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {HsHomePageComponent} from './hs-home-page/hs-home-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

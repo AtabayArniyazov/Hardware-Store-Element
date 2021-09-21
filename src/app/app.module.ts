d7ad5c413efbc21eec1ae92081f022dd83625855
Atabay <Atabaj.Arniyazov@p97.com>
2021-09-21 23:55:41 +0300

    create hs-header.component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HsHeaderComponent } from './hs-header/hs-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HsHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hs-header/hs-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hs-header',
+  templateUrl: './hs-header.component.html',
+  styleUrls: ['./hs-header.component.css']
+})
+export class HsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
